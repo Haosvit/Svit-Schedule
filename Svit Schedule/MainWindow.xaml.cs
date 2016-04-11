18a7617cadf0548e43262cd70e1089dbf3338548
Hao Svit <haonguyen.svit@gmail.com>
2016-04-11 10:20:45 +0700

    + created project; + Sketched GUI

---
+++ Svit Schedule/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Svit_Schedule
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string btnName = btn.Name;
+
+            switch (btnName)
+            {
+                case "mon1":
+                    MessageBox.Show(((TextBlock)btn.Content).Text);
+                    break;
+                case "mon2":
+                    MessageBox.Show(((TextBlock)btn.Content).Text);
+                    break;
+                case "mon3":
+                    MessageBox.Show(((TextBlock)btn.Content).Text);
+                    break;
+                case "sun1":
+                    MessageBox.Show("Sunday task");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
